e5683dbf6965be8d19dd7978a0ca44bc23efc508
Sam (Protosus) <sam.avanesoff@gmail.com>
2025-07-19 17:07:30 -0400

    Ov layout

--- src/components/VodService/VodAsset.tsx
+++ src/components/VodService/VodAsset.tsx
@@ -2,13 +2,20 @@ import type { VodAsset } from "../../hooks/vod";
 
 export const VodAssetView = (data: VodAsset) => {
   return (
-    <div>
-      <h3>{data.title}</h3>
-      <p>{data.description}</p>
-      <img src={data.thumbnailUrl} alt={data.title} />
-      <p>Duration: {data.duration} seconds</p>
-      <p>Published At: {data.publishedAt}</p>
-      {data.genre && <p>Genre: {data.genre.join(", ")}</p>}
+    <div className="rounded border p-4 bg-cyan-50 flex flex-col gap-2">
+      <div className="font-bold  bg-amber-200">{data.title}</div>
+      <div className="w-full overflow-ellipsis overflow-hidden text-nowrap">
+        {data.description}
+      </div>
+      <img src={data.thumbnailUrl} alt={data.title} className="size-32" />
+      {/* <p>Duration: {data.duration} seconds</p>
+      <p>Published At: {data.publishedAt}</p> */}
+      <div
+        className="flex flex-row
+        items-center gap-2 text-sm text-gray-600"
+      >
+        {data.genre && <p>Genre: {data.genre.join(", ")}</p>}
+      </div>
     </div>
   );
 };
--- src/components/VodService/VodService.tsx
+++ src/components/VodService/VodService.tsx
@@ -7,11 +7,13 @@ export const VodService: React.FC = () => {
   const { data, ...query } = useVod();
   return (
     <ServiceCard title="VOD Service" status={query}>
-      <div>
+      <div className="flex flex-col gap-4">
         <h2>VOD List:</h2>
-        {data?.vod.map((asset) => (
-          <VodAssetView key={asset.id} {...asset} />
-        ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data?.vod.map((asset) => (
+            <VodAssetView key={asset.id} {...asset} />
+          ))}
+        </div>
       </div>
     </ServiceCard>
   );
