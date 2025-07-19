b2fa1d14a229d9100fc27682689f9bdf3865d617
Sam (Protosus) <sam.avanesoff@gmail.com>
2025-07-19 14:40:20 -0400

    query chain defined

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./hooks/auth/AuthProvider";
 import { ServiceDemo } from "./components/ServiceDemo";
 import { AppConfigProvider } from "./hooks/AppConfig";
+import { AccountProvider } from "./hooks/Account/AccountProvider";
+import { StandbyProvider } from "./hooks/standby/StandbyProvider";
+import { EnvProvider } from "./hooks/env/EnvProvider";
 
 const queryClient = new QueryClient();
 
@@ -9,9 +12,15 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppConfigProvider>
-        <AuthProvider>
-          <ServiceDemo />
-        </AuthProvider>
+        <EnvProvider invalidateQueries={["auth"]} env="staging">
+          <AuthProvider invalidateQueries={["account"]}>
+            <AccountProvider invalidateQueries={["vod"]}>
+              <StandbyProvider invalidateQueries={["auth"]}>
+                <ServiceDemo />
+              </StandbyProvider>
+            </AccountProvider>
+          </AuthProvider>
+        </EnvProvider>
       </AppConfigProvider>
     </QueryClientProvider>
   );
